c79e19d2ff4dd09e0fc37040dab35183cbfd97b8
Chris Tsui <chris.tsui112@outlook.com>
2020-08-25 11:06:12 -0700

    added apple

--- code.js
+++ code.js
@@ -8,10 +8,34 @@ let initialStart = true;
 let currentDirection= "right";
 let deltaX= 5;
 let deltaY= 5;
+let snakeWidth = 25;
+let snakeHeight = 25;
+let snakeCurrentPosition = {x1:xCoordinate,
+                            x2:xCoordinate+snakeHeight,
+                            y1:yCoordinate,
+                            y2:yCoordinate+snakeHeight
+}
+createApple();
 setInterval(()=>{
     drawSnake();
     snakeMovement();
     },100);
+
+function createApple(){
+    // coordinates are in the center of the circle
+    let xCoordinateApple= Math.floor(Math.random() * (750-50+1)+50);
+    let yCoordinateApple=Math.floor(Math.random() * (750-50+1)+50);
+    // while((snakeCurrentPosition["x1"]<xCoordinateApple<snakeCurrentPosition["x2"])&& (snakeCurrentPosition["y1"]<xCoordinateApple<snakeCurrentPosition["y2"])){
+    //     let xCoordinateApple= Math.floor(Math.random() * (750-50+1)+50);
+    //     let yCoordinateApple=Math.floor(Math.random() * (750-50+1)+50);
+    //
+    // }
+    //ctx.beginPath();
+    ctx.arc(xCoordinateApple,yCoordinateApple,100,0,Math.PI*2);
+    ctx.fillStyle ="red";
+    ctx.fill();
+    //ctx.closePath();
+}
 
 function snakeMovement(){
     if (currentDirection === "right"){
@@ -56,12 +80,12 @@ function changeDirection(keyPress){
 }
 
 function drawSnake() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
     ctx.fillStyle = 'black';
     ctx.fillRect(0,0, canvas.width, canvas.height);
     ctx.fillStyle = 'green';
-    ctx.fillRect(xCoordinate, yCoordinate, 25, 25);
+    ctx.fillRect(xCoordinate, yCoordinate, snakeWidth, snakeHeight);
     ctx.closePath();
 }
 
