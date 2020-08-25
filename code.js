8aaebdf84a447eb0e04d00654450abf544aaf055
Chris Tsui <chris.tsui112@outlook.com>
2020-08-25 10:09:58 -0700

    direction and movement functions started

--- code.js
+++ code.js
@@ -2,24 +2,64 @@ const canvas = document.getElementById("snakeGameBoard");
 const ctx = canvas.getContext("2d");
 ctx.fillStyle = 'black';
 ctx.fillRect(0,0, canvas.width, canvas.height);
-let deltaX;
-let deltaY;
 let xCoordinate=50;
 let yCoordinate=400;
+let initialStart = true;
+let currentDirection= "right";
+let deltaX= 5;
+let deltaY= 5;
 setInterval(()=>{
     drawSnake();
-    moveSnake();
-
-
-},100);
-
-
-function moveSnake(){
-    let deltaX= 5;
-    let deltaY= 5;
-    xCoordinate+=deltaX;
+    movement();
+    },100);
+function movement(){
+    if (currentDirection === "right"){
+        xCoordinate += deltaX;
+        return;
+    }
+    if (currentDirection === "up"){
+        yCoordinate -= deltaY;
+        return;
+}
 
 }
+function moveSnake(arrow){
+
+    let direction = arrow;
+    const directionTranslation = { up:38,
+                                 down:40,
+                                 left:37,
+                                 right:39
+    }
+    // if (initialStart === true){
+    //     xCoordinate+=deltaX;
+    //     return;
+    // }
+    if (direction === directionTranslation["up"]){
+        yCoordinate -= deltaY;
+        currentDirection = "up";
+        initialStart=false;
+        return;
+    }
+    if (direction === directionTranslation["down"]){
+        yCoordinate += deltaY;
+        initialStart=false;
+        return;
+    }
+    if (direction === directionTranslation["left"]){
+        xCoordinate -= deltaX;
+        initialStart=false;
+        return;
+    }
+    if (direction === directionTranslation["right"]){
+        xCoordinate += deltaX;
+        currentDirection ="right"
+        initialStart=false;
+        return;
+    }
+
+}
+
 function drawSnake() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
@@ -29,4 +69,9 @@ function drawSnake() {
     ctx.fillRect(xCoordinate, yCoordinate, 25, 25);
     ctx.closePath();
 }
+
+document.addEventListener("keydown",(event)=>{
+     let direction = event.which;
+    moveSnake(direction);
+})
 
